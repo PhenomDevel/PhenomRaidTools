6d2b4f1f0247d67d0f48729cce41250016ac2440
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2020-07-26 21:40:34 +0200

    Add shared media files

---
+++ PhenomRaidTools/Source/Media.lua
@@ -0,0 +1,9 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+if not LSM then 
+   return 
+end
+
+local folder = [[Interface\Addons\PhenomRaidTools\Media\Sounds\]]
+
+-- LSM:Register("sound", "EN-Female-ConcentrateForOnce", folder .. [[en_female_concentrate_once_please.ogg]])
