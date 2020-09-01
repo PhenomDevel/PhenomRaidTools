9a7fb05ef95d99c2c0a4f5665b8d13c5a6ebb9ac
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2020-09-01 10:34:56 +0200

    Correctly add default sound to collection

--- PhenomRaidTools/Source/Media.lua
+++ PhenomRaidTools/Source/Media.lua
@@ -8,4 +8,4 @@ local folder = [[Interface\Addons\PhenomRaidTools\Media\Sounds\]]
 
 LSM:Register("sound", "DancingQueen", folder .. [[DancingQueen.ogg]])
 LSM:Register("sound", "LaufSLauf", folder .. [[run-bitch-run.ogg]])
-LSM:Register("sound", "PRT:Default", folder .. "[[ReceiveMessage.ogg]]")
+LSM:Register("sound", "PRT: Default", folder .. [[ReceiveMessage.ogg]])
