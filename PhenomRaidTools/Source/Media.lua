520e44ab64098eff885b2f5e546e759241f59ce1
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2020-09-22 19:27:17 +0200

    Changed media names

--- PhenomRaidTools/Source/Media.lua
+++ PhenomRaidTools/Source/Media.lua
@@ -6,6 +6,6 @@ end
 
 local folder = [[Interface\Addons\PhenomRaidTools\Media\Sounds\]]
 
-LSM:Register("sound", "DancingQueen", folder .. [[DancingQueen.ogg]])
-LSM:Register("sound", "LaufSLauf", folder .. [[run-bitch-run.ogg]])
+LSM:Register("sound", "PRT: DancingQueen", folder .. [[DancingQueen.ogg]])
+LSM:Register("sound", "PRT: LaufSLauf", folder .. [[run-bitch-run.ogg]])
 LSM:Register("sound", "PRT: Default", folder .. [[ReceiveMessage.ogg]])
