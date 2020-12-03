592e4fedd9ad8bc35d80f243e5a8e1803a718a7d
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2020-12-03 22:29:14 +0100

    Be able to use $target as message target again

--- PhenomRaidTools/Source/Utils.lua
+++ PhenomRaidTools/Source/Utils.lua
@@ -452,20 +452,22 @@ PRT.ExchangeSpellIcons = function(s)
 end
 
 PRT.AddCustomPlaceholdersToPlayerNames = function(token, t, customPlaceholders)
-    for i, customPlaceholder in ipairs(customPlaceholders) do
-        if customPlaceholder.name == token then
-            if customPlaceholder.type == "group" then
-                for i = #customPlaceholder.names, 1, -1 do
-                    tinsert(t, strtrim(customPlaceholder.names[i], " "))
-                end
-            else
-                for nameIdx, name in ipairs(customPlaceholder.names) do
-                    if PRT.UnitInParty(name) or UnitExists(name) then
-                        tinsert(t, strtrim(name, " "))
-                        break
+    if customPlaceholders then
+        for i, customPlaceholder in ipairs(customPlaceholders) do
+            if customPlaceholder.name == token then
+                if customPlaceholder.type == "group" then
+                    for i = #customPlaceholder.names, 1, -1 do
+                        tinsert(t, strtrim(customPlaceholder.names[i], " "))
                     end
-                end
-            end                
+                else
+                    for nameIdx, name in ipairs(customPlaceholder.names) do
+                        if PRT.UnitInParty(name) or UnitExists(name) then
+                            tinsert(t, strtrim(name, " "))
+                            break
+                        end
+                    end
+                end                
+            end
         end
     end
 end
