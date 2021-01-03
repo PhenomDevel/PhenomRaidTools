fb2262a73e67d780fb16cd19a125d58c6b587e88
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2021-01-04 00:23:34 +0100

    Add innervate sound

--- PhenomRaidTools/Source/Media.lua
+++ PhenomRaidTools/Source/Media.lua
@@ -11,6 +11,7 @@ LSM:Register("sound", "PRT: LaufSLauf", folder .. [[run-bitch-run.ogg]])
 LSM:Register("sound", "PRT: Default", folder .. [[ReceiveMessage.ogg]])
 LSM:Register("sound", "PRT: Ramp", folder .. [[Ramp.ogg]])
 LSM:Register("sound", "PRT: Power Infusion", folder .. [[PowerInfusion.ogg]])
+LSM:Register("sound", "PRT: Innervate", folder .. [[Innervate.ogg]])
 
 -- Healing CDs
 LSM:Register("sound", "PRT: Barrier", folder .. [[Barrier.ogg]])
