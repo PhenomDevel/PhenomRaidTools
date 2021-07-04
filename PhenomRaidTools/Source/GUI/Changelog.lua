eadb299e2410954d708b21352840dbfbe1dbca13
Kevin <13941819+PhenomDevel@users.noreply.github.com>
2021-07-04 14:16:33 +0200

    Update Changelog.lua

--- PhenomRaidTools/Source/GUI/Changelog.lua
+++ PhenomRaidTools/Source/GUI/Changelog.lua
@@ -3,10 +3,10 @@ local L = LibStub("AceLocale-3.0"):GetLocale("PhenomRaidTools")
 
 local changelogLines = {
   PRT.ColoredString(L["Latest Features"], PRT.Static.Colors.Primary),
-  PRT.ColoredString(L["Ability to export timers as ExRT note"], PRT.Static.Colors.Secondary),
-  L["- Export any timer as ExRT note"],
+  PRT.ColoredString(L["Ability to export timers as MethodRaidTools note"], PRT.Static.Colors.Secondary),
+  L["- Export any timer as MethodRaidTools note"],
   L["- %s This only works for messages with type %s"]:format(PRT.ColoredString("(!)", PRT.Static.Colors.Disabled), PRT.HighlightString(L["cooldown"])),
-  L["- To do so you have to navigate to %s within any encounter and click %s"]:format(PRT.HighlightString(L["Timers"]), PRT.HighlightString(L["Generate ExRT Note"])),
+  L["- To do so you have to navigate to %s within any encounter and click %s"]:format(PRT.HighlightString(L["Timers"]), PRT.HighlightString(L["Generate MethodRaidTools Note"])),
   "",
   PRT.ColoredString(L["Spell database options tab"], PRT.Static.Colors.Secondary),
   L["- Within the spell database you can search any spell the game has to over"],
@@ -23,7 +23,7 @@ local changelogLines = {
 function PRT.AddChangelogWidgets(container)
   local changelogGroup = PRT.InlineGroup(L["Changelog"])
   local changelogString = strjoin("\n", unpack(changelogLines))
-  local changeLogLabel = PRT.Label(changelogString, 16)
+  local changeLogLabel = PRT.Label(changelogString, 14)
 
   changeLogLabel:SetRelativeWidth(1)
 
